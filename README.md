d3e388a08536ac65339c75cb0f3aeb09f31c181a
zhengxy03 <1730861545@qq.com>
2024-10-10 11:09:18 +0800

    modified

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ubuntu
 * brew.sh
+    ** install CLI applications
+    
--- brew.sh
+++ brew.sh
@@ -19,3 +19,119 @@ else
         brew unlink pkg-config
     fi
 fi
+
+# perl
+echo "==> install perl 5.34"
+brew install perl
+
+if grep -q -i PERL_534_PATH $HOME/*.bashrc ; then
+    echo "==> .bashrc already contains PERL_534_PATH"
+else
+    echo "==> updating .bashrc with PERL_534_PATH"
+    PERL_534_BREW=$(brew --prefix)/Cellar/$(brew list --versions perl | sed 's/ /\//' | head -n 1)
+    PERL_534_PATH="export PATH=\"PERL_534_BREW/bin:\$PATH\""
+    echo '# PERL_534_PATH' >> $HOME/.bashrc
+    echo $PERL_534_PATH >> $HOME/.bashrc
+    echo >> $HOME/.bashrc
+
+    eval $PERL_534_PATH
+fi
+
+hash capnm 2>/dev/null || {
+    curl -L --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ APP::capnminus
+}
+
+# building tools
+echo "==> building tools"
+brew install autoconf libtool automake # autogen
+brew install cmake
+brew install bison flex
+
+# libs
+brew install gd gsl jemalloc boost # fftw
+brew install libffi libgit2 libxml2 libgcrypt libxslt
+brew install pcre libedit readline sqlite nasm yasm
+brew install bzip2 gzip libarchive libzip xz
+
+# python
+brew install python@3.9
+
+if grep -q -i PYTHON_39_PATH $HOME/.bashrc; then
+    echo "==> .bashrc contains PATHON_39_PATH"
+else
+    echo "==> updating .bashrc with PATHON_39_PATH"
+    PATHON_39_PATH="export PATH=\"$(brew --prefix)/opt/python@3.9/bin:$(brew --prefix)/opt/python@3.9/libexec/bin:\$PATH\""
+    echo '# PYTHON_39_PATH' >> $HOME/.bashrc
+    echo ${PYTHON_39_PATH} >> $HOME/.bashrc
+    echo >> $HOME/.bashrc
+
+    eval ${PATHON_39_PATH}
+fi
+
+# pip3 install --upgrade pip setuptools wheel
+
+# R
+# brew install wang-q/tap/r@3.6.1
+hash R 2>/dev/null || {
+    echo "==> install R"
+    brew install r
+}
+capnm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest Statistics::R
+
+# java
+if [["OSTYPE"=="darwin"* ]]; then
+    brew install openjdk
+else
+    brew install openjdk
+fi
+brew install ant maven
+
+# pin these
+# brew pin perl
+# brew pin python@3.9
+# brew pin r
+
+# other programming languages
+brew install lua node
+
+# taps
+brew tap wang-q/tap
+
+# downloading tools
+brew install aria2 curl wget
+
+# gnu
+brew install gnu-sed gnu-tar
+
+# other tools
+brew install screen stow htop parallel pigz
+brew install tree pv
+brew install jq jid pup
+brew install datamash miller tsv-utils
+brew install librsvg udunits
+brew install proxychains-ng
+
+brew install bat tealdeer # exa tiv
+brew install hyperfine ripgrep tokei
+brew install bottom # zellij
+
+# large packages
+if [["OSTYPE"=="darwin"]]; then
+    brew install gpg2
+fi
+
+hash pandoc 2>/dev/null || {
+    brew install pandoc
+}
+
+hash gnupolt 2>/dev/null || {
+    brew install gunplot
+}
+
+hash dot 2>/dev/null || {
+    brew install graphviz
+}
+
+hash convert 2>/dev/null || {
+    brew install Imagemagick
+}
