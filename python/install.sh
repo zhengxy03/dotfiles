d8c3edc8fde9b59a81bbcf3ab0e511e3348a9044
zhengxy03 <1730861545@qq.com>
2024-10-13 23:09:49 +0800

    new file

---
+++ python/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )
+
+cd "${BASE_DIR}" || exit
+
+PIPY_MIRROR=https://mirrors.nju.edu.cn/pypi/web/simple
+
+pip3 install -i ${PIPY_MIRROR} pysocks cryptography
+pip3 install -i ${PYPI_MIRROR} virtualenv
+pip3 install -i ${PYPI_MIRROR} more-itertools zipp setuptools-scm
+pip3 install -i ${PYPI_MIRROR} numpy matplotlib
+pip3 install -i ${PYPI_MIRROR} pandas scipy jupyter sympy
+pip3 install -i ${PYPI_MIRROR} lxml statsmodels patsy h5py
+pip3 install -i ${PYPI_MIRROR} beautifulsoup4 scikit-learn seaborn
+
+#HDF5_DIR=$(brew --prefix hdf5) pip install h5py tables
+
+pip3 install -i ${PYPI_MIRROR} h5py tabulate # TBB
+
+# PPanGGOLiN
+pip3 install -i ${PYPI_MIRROR} tqdm tables networkx dataclasses
+pip3 install -i ${PYPI_MIRROR} plotly gmpy2 colorlover bokeh
+
+# antismash
+pip3 install -i ${PYPI_MIRROR} helperlibs jinja2 joblib jsonschema markupsafe pysvg pyscss
+pip3 install -i ${PYPI_MIRROR} biopython bcbio-gff
+
+pip3 install -i ${PYPI_MIRROR} Circle-Map cutadapt importlib-metadata
+
+pip3 install -i ${PYPI_MIRROR} deeptools
